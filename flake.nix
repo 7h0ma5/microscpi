6e0ae66cd181252aaa18ec8f1cde1df68a0f6dc2
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2025-05-25 10:31:33 +0200

    chore: update dependencies and development environment
    - Update Rust dependencies in Cargo.lock (criterion 0.6, defmt 1.0, etc.)
    - Update Nix flake to nixos-25.05 and add cargo-nextest
    - Refresh flake.lock with latest versions

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
@@ -8,19 +8,25 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix, ... } :
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    fenix,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ fenix.overlays.default ];
+          overlays = [fenix.overlays.default];
         };
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [
             pkgs.libiconv
             pkgs.cargo-expand
+            pkgs.cargo-nextest
             pkgs.cargo-release
             pkgs.cargo-fuzz
             (pkgs.fenix.stable.withComponents [
@@ -34,5 +40,5 @@
           ];
         };
       }
-  );
+    );
 }
